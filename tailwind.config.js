b059ee0302d6b18d44d2a596781718c86d53c15d
HT-Dat <hotiendat661@gmail.com>
2022-03-20 02:47:20 +0700

    Update version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-    mode: 'jit',
-    purge: ['./src/**/*.js', './public/index.html'],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        fontFamily: {
-            sans: ['Roboto', 'sans-serif'],
-            serif: ['"Roboto Slab"', 'serif'],
-            body: ['Roboto', 'sans-serif'],
-        },
-        extend: {},
+  content: ["./src/**/*.js", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      serif: ['"Roboto Slab"', "serif"],
+      body: ["Roboto", "sans-serif"],
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+    extend: {},
+  },
+  plugins: [],
 };
