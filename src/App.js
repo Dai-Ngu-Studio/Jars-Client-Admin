7a9fad54de1fc46d47f0ffadeb4657834ea6703a
HT-Dat <hotiendat661@gmail.com>
2022-03-20 02:48:17 +0700

    New route and protect by authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Sidebar from 'components/Sidebar';
-import Dashboard from 'pages/Dashboard';
-import Tables from 'pages/Tables';
-import Footer from 'components/Footer';
-
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./pages/Dashboard";
+import Footer from "components/Footer";
+import Login from "./pages/Login";
+import AuthProvider from "./context/AuthProvider";
+import Table from "./pages/Tables";
 // Tailwind CSS Style Sheet
-import 'assets/styles/tailwind.css';
-
+import "assets/styles/tailwind.css";
+function _Dashboard() {
+  return (
+    <>
+      <Sidebar />
+      <div className="md:ml-64">
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/overview" />
+          </Route>
+          <Route component={Dashboard} path="/overview" />
+          <Route component={Table} path="/table" />
+        </Switch>
+        <Footer />
+      </div>
+    </>
+  );
+}
 function App() {
-    return (
-        <>
-            <Sidebar />
-            <div className="md:ml-64">
-                <Switch>
-                    <Route exact path="/" component={Dashboard} />
-                    <Route exact path="/tables" component={Tables} />
-                    <Redirect from="*" to="/" />
-                </Switch>
-                <Footer />
-            </div>
-        </>
-    );
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={_Dashboard} />
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
